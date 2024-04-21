025aeb1072aeca227d54f4bd71a94cb318426b68
Souken521 <west.haru6555@gmail.com>
2024-04-21 20:45:31 +0900

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,16 @@
+// 参考:https://tomoyuki65.com/how-to-develop-responsive-design-screens-with-nextjs13/
 import Image from "next/image";
 import Link from "next/link";
 import BurgerMenu from "@/components/burgerMenu";
 export const Header = () => {
     return (
         // <header className="flex items-center m-3">
-        //     <div className="flex lg:flex-1 mr-auto items-center gap-3">
+        //     <div className="flex md:flex-1 mr-auto items-center gap-3">
         //         <Link href="/" className="text-5xl">
         //             Souken521
         //         </Link>
         //     </div>
-        //     <div className="flex mx-4 lg:flex-1 lg:gap-x-12 justify-end">
+        //     <div className="flex mx-4 md:flex-1 md:gap-x-12 justify-end">
         //         <Link href="/" className="font-semibold">Home</Link>
         //         <Link href="/" className="font-semibold">About</Link>
         //         <Link href="/" className="font-semibold">Projects</Link>
@@ -18,21 +19,21 @@ export const Header = () => {
         // </header>
 
     <header className="w-full sticky top-0">
-            <div className="hidden md:flex justify-between min-w-3xl items-center m-3">
-                <div className="flex lg:flex-1 mr-auto items-center gap-3">
+            <div className="hidden md:flex justify-between min-w-3xl items-center mx-3">
+                <div className="flex md:flex-1 mr-auto items-center gap-3">
                     <Link href="/" className="text-5xl">
                         Souken521
                     </Link>
                 </div>
-                <div className="flex mx-4 lg:flex-1 lg:gap-x-12 justify-end">
+                <div className="flex mx-4 md:flex-1 md:gap-x-12 justify-end">
                     <Link href="/" className="font-semibold">Home</Link>
                     <Link href="/" className="font-semibold">About</Link>
                     <Link href="/" className="font-semibold">Projects</Link>
                     <Link href="/" className="font-semibold">Blog</Link>
                 </div>
             </div>
-            <div className="md:hidden flex justify-between max-w-3xl">
-                <div className="text-xl font-bold">
+            <div className="md:hidden flex justify-between max-w-3xl  mx-3">
+                <div>
                     <Link href="/" className="text-5xl">
                         Souken521
                     </Link>

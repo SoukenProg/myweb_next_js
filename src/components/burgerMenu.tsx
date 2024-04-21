78f31aca0142169a913eb2d06b6972ddaad52ee6
Souken521 <west.haru6555@gmail.com>
2024-04-21 19:27:35 +0900

    Add burgerMenu

---
+++ src/components/burgerMenu.tsx
@@ -0,0 +1,76 @@
+'use client'
+import { useState } from 'react';
+import { slide as Menu, State } from 'react-burger-menu'
+import Link from "next/link";
+
+const styles = {
+    bmBurgerButton: {
+        position: 'fixed',
+        width: '32px',
+        height: '26px',
+        right: '20px',
+        top: '5px',
+    },
+    bmBurgerBars: {
+        background: 'purple'
+    },
+    bmCross: {
+        background: 'white'
+    },
+    bmMenuWrap: {
+        position: 'fixed',
+        height: '100%',
+        top: "0",
+    },
+    bmMenu: {
+        background: 'dimgray',
+        padding: '2.5em 1.5em 0',
+    },
+    bmItemList: {
+        color: 'white',
+    },
+}
+export default function BurgerMenu() {
+    // メニュー画面の開閉状態を管理する「isMenuOpen」を設定
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    // メニューを閉じるための関数を設定
+    const handleCloseMenu = () => {
+        setIsMenuOpen(false)
+    }
+
+    // メニュー画面の開閉状態を確認するための関数を設定
+    const handleStateChange = (state: State) => {
+        setIsMenuOpen(state.isOpen)
+    }
+
+    return (
+        <div id="outer-container">
+            <Menu
+                isOpen={isMenuOpen}
+                onStateChange={handleStateChange}
+                pageWrapId={'page-wrap'}
+                outerContainerId={'outer-container'}
+                right={true}
+                styles={styles}
+            >
+                <main id="page-wrap">
+                    <ul className="flex flex-col">
+                        <li className="mb-4">
+                            <Link href="/" className="font-semibold">Home</Link>
+                        </li>
+                        <li className="mb-4">
+                            <Link href="/" className="font-semibold">About</Link>
+                        </li>
+                        <li className="mb-4">
+                            <Link href="/" className="font-semibold">Projects</Link>
+                        </li>
+                        <li className="mb-4">
+                            <Link href="/" className="font-semibold">Link</Link>
+                        </li>
+                    </ul>
+                </main>
+            </Menu>
+        </div>
+    )
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,48 @@
 import Image from "next/image";
 import Link from "next/link";
-
+import BurgerMenu from "@/components/burgerMenu";
 export const Header = () => {
-    return(
-        <header>
-            <div className="flex lg:flex-1  ">
-                <Link href="/"　className="text-2xl font-logo">
-                    Souken521
-                </Link>
+    return (
+        // <header className="flex items-center m-3">
+        //     <div className="flex lg:flex-1 mr-auto items-center gap-3">
+        //         <Link href="/" className="text-5xl">
+        //             Souken521
+        //         </Link>
+        //     </div>
+        //     <div className="flex mx-4 lg:flex-1 lg:gap-x-12 justify-end">
+        //         <Link href="/" className="font-semibold">Home</Link>
+        //         <Link href="/" className="font-semibold">About</Link>
+        //         <Link href="/" className="font-semibold">Projects</Link>
+        //         <Link href="/" className="font-semibold">Blog</Link>
+        //     </div>
+        // </header>
+
+    <header className="w-full sticky top-0">
+            <div className="hidden md:flex justify-between min-w-3xl items-center m-3">
+                <div className="flex lg:flex-1 mr-auto items-center gap-3">
+                    <Link href="/" className="text-5xl">
+                        Souken521
+                    </Link>
+                </div>
+                <div className="flex mx-4 lg:flex-1 lg:gap-x-12 justify-end">
+                    <Link href="/" className="font-semibold">Home</Link>
+                    <Link href="/" className="font-semibold">About</Link>
+                    <Link href="/" className="font-semibold">Projects</Link>
+                    <Link href="/" className="font-semibold">Blog</Link>
+                </div>
             </div>
-        </header>
-    )
+            <div className="md:hidden flex justify-between max-w-3xl">
+                <div className="text-xl font-bold">
+                    <Link href="/" className="text-5xl">
+                        Souken521
+                    </Link>
+                </div>
+                <div className="text-xl font-bold">
+                    <BurgerMenu />
+                </div>
+            </div>
+    </header>
+)
 }
 
-export  default Header;
+export default Header;
